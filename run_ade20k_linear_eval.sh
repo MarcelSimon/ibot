0a0655603e5f16e4fd4b1e7f98ad55e099e6d433
Marcel Simon <9692715+MarcelSimon@users.noreply.github.com>
2025-03-31 18:37:06 +0900

    Updated ade20k sample script

--- run_ade20k_linear_eval.sh
+++ run_ade20k_linear_eval.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 set -e 
 
+# Configuration
 IBOT_DIR=/path/to/ibot
-
-#!/bin/bash
-
-# Define clear variables for maintainability
-IMAGE_NAME="ibot_docker"
+IMAGE_NAME="marcel.simon_ibot"
 CONTAINER_NAME="$IMAGE_NAME"
 BASE_DIR=/path/to/pre-trained/model
-CHECKPOINT=$BASE_DIR/checkpoint.pth
+CHECKPOINT=$BASE_DIR/checkpoint.final.pth
 OUTPUT_DIR=$BASE_DIR/ade20k-results
 DATA_ROOT=/path/to/ADEChallengeData2016
+VISIBLE_GPUS=0,1,2,3
 
-# Run Docker container with explicit merged parameters
+# Script starts here
+NUM_GPUS=$(echo "${VISIBLE_GPUS}" | awk -F',' '{print NF}')
 
-docker run \
+mkdir -p "/${OUTPUT_DIR}/seg"
+cp "$(realpath "$0")" "${OUTPUT_DIR}/seg/" \
+&& docker run \
     -it \
     --rm \
     --init \
-    --gpus '"device=1,2,3,4"' \
+    --gpus "\"device=$VISIBLE_GPUS\"" \
     --ipc=host \
     --ulimit memlock=-1 \
     --ulimit stack=67108864 \
-    -e PRETRAINED=/data/$CHECKPOINT \
-    -e OUTPUT_DIR=/data/$OUTPUT_DIR \
+    -e PRETRAINED="/data/$CHECKPOINT" \
+    -e OUTPUT_DIR="/data/$OUTPUT_DIR" \
     -e "LOCAL_UID=$(id -u)" \
     -e "LOCAL_GID=$(id -g)" \
     -v /:/data \
     --name $CONTAINER_NAME \
     $IMAGE_NAME \
     /bin/sh -c "\
-        mkdir -p \"/data/$OUTPUT_DIR\" \
-        && cd /data/$IBOT_DIR/ \
+        cd \"/data/${IBOT_DIR}/\" \
         && bash run.sh \
             ade20k_seg \
             vino_ade20k_seg_eval \
             vit_small \
             teacher \
-            2 \
+            $NUM_GPUS \
             data.samples_per_gpu=4 \
             data.workers_per_gpu=4 \
             model.backbone.out_with_norm=true \
@@ -46,4 +46,5 @@ docker run \
             data.train.data_root=/data/$DATA_ROOT \
             data.val.data_root=/data/$DATA_ROOT \
             data.test.data_root=/data/$DATA_ROOT \
+            2>&1 | tee -a \"/data/${OUTPUT_DIR}/seg/semseg.log\" \
         "
